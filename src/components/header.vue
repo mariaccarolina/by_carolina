052ce73a6c8c5e0225bbd53aae82bf08af33d8ba
Maria Carolina <demycar24@gmail.com>
2025-01-28 19:50:30 -0300

    first commit

---
+++ src/components/header.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+    <header>
+        <h2>Carolina Oliveira</h2>
+        <nav>
+            <ul>
+                <li>Início</li>
+                <li>Sobre mim</li>
+                <li>Experiências</li>
+                <li>Projetos</li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style>
+
+</style>
