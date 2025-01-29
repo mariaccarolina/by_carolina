657d0d366781fc2a314500e6f4b7f5daef0e59d4
Maria Carolina <demycar24@gmail.com>
2025-01-29 09:37:46 -0300

    :lipstick: feat: Estilização @media header

--- src/components/header.vue
+++ src/components/header.vue
@@ -17,24 +17,34 @@
 <style lang="scss">
 header {
   background-color: #0b0b0b;
-  color: #FFFFFF;
+  color: #ffffff;
   display: flex;
   align-items: center;
+  height: 12vh;
   justify-content: space-around;
+  border-bottom-style: solid #151515;
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+    justify-content: center;
+  }
 }
 h2 {
   color: #ed91cb;
 }
 
-ul{
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    width: 40vw;
-    
-    li{
-        list-style: none;
-        cursor: pointer;
-    }
+ul {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 40vw;
+  @media (max-width: 900px) {
+    width: 80vw;
+  }
+
+  li {
+    list-style: none;
+    cursor: pointer;
+  }
 }
 </style>
