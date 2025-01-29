c68bee950d5b12209b8e24849daa548c02d93ff3
Maria Carolina <demycar24@gmail.com>
2025-01-29 09:59:28 -0300

    :sparkles: feat: Página de inicio

---
+++ src/components/inicio.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+    <section>
+        <div>
+            <p>Olá eu sou</p>
+            <h1>Carolina</h1>
+            <p>Desenvolvedora Frontend e Instrutora Técnica</p>
+            <button>LikedIn</button>
+        </div>
+    </section>
+    <figure>
+        <img src="" alt="">
+    </figure>
+</template>
