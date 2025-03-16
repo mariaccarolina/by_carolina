c26cefe6524fd6612b77eecc366d2367111b9770
Maria Carolina <demycar24@gmail.com>
2025-03-16 19:37:54 -0300

    :sparkles: feat: Projetos

---
+++ src/components/projetosCard.vue
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<template>
+    <section>
+        <div class="project-card">
+          <img src="" alt="">  
+          <div>
+            <h3></h3>
+            <p></p>
+            <div>
+                <h4></h4>
+                <h4></h4>
+                <h4></h4>
+            </div>
+          </div>
+        </div>
+    </section>
+</template>
